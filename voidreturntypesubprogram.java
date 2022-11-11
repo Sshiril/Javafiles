bf3895a402c0fd38f2b50c4224097a1f0c179ec2
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-11-10 21:56:58 -0500

    Create voidreturntypesubprogram.java

---
+++ voidreturntypesubprogram.java
@@ -0,0 +1,21 @@
+//Program with return type as void for 
+public class subprograms {
+
+    public static void main(String[] args) { //main function
+        int a=5, b=6,answer; //initializing inside main function
+     
+        myMethod(a,b); // calling the function
+    
+        
+        
+    }
+
+    private static void myMethod(int x, int y) {   //subprogram
+      int c;
+      c=x+y;  // Calculating the sum of two integers
+    
+      System.out.println(c);  // printing sum   //returning nothing that's why void return type
+      
+    }
+    
+}
